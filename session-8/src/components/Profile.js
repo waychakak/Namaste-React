cc07ae241ce868a0c4210fa5be8fb174dd400a19
Akshay Ashok Waychal <akshaywaychal56@gmail.com>
2023-05-02 18:23:05 +0530

    session-8

--- session-8/src/components/Profile.js
+++ session-8/src/components/Profile.js
@@ -4,17 +4,39 @@ class Profile extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-            count: 0,
+            userInfo: {
+                name: "Dummy Name",
+                location: "Dummy Location",
+            },
         };
+        console.log("constructor")
     }
 
+    async componentDidMount() {
+        const data = await fetch('https://api.github.com/users/akshaymarch7');
+        const json = await data.json();
+        console.log(json);
+        this.setState({
+            userInfo: json,
+        }); 
+        console.log("componentDidMount");
+    }
+
+    componentDidUpdate() {
+        console.log('compoentDIdUpdate');
+    }
+
+    componentWillUnmount() {
+        //clearInterval()
+    }
 
     render() {
+        console.log("render");
         return (
         <> 
             <h1>Profile Class Component</h1>
-            <h2>Name: {this.props.name}</h2>
-            <h3>Count : {this.state.count}</h3>
+            <h2>Name: {this?.state?.userInfo?.name}</h2>
+            <h3>Location : {this?.state?.userInfo?.location}</h3>
         </>
         )
     }
